aa59bae0932070f31b7aea623c0c3c62fbaeb48f
EagleVee <nnavu1@gmail.com>
2019-09-16 17:53:23 +0700

    add create variants query

---
+++ create_variant/create_variants.js
@@ -0,0 +1,100 @@
+const fs = require('fs');
+const products = [
+  "122ec6f4-76cf-44aa-bdf1-df7fd7b71a2b",
+  "222ec6f4-76cf-44aa-bdf1-df7fd7b71a2b",
+  "322ec6f4-76cf-44aa-bdf1-df7fd7b71a2b",
+  "422ec6f4-76cf-44aa-bdf1-df7fd7b71a2b",
+  "522ec6f4-76cf-44aa-bdf1-df7fd7b71a2b",
+  "622ec6f4-76cf-44aa-bdf1-df7fd7b71a2b"
+]
+
+const volumns = [
+  {
+    "variant_key": "volumn",
+    "variant_value": "100",
+    "display_value": "100ml"
+  },
+  {
+    "variant_key": "volumn",
+    "variant_value": "300",
+    "display_value": "300ml"
+  },
+  {
+    "variant_key": "volumn",
+    "variant_value": "500",
+    "display_value": "500ml"
+  }
+];
+
+const weights = [
+  {
+    "variant_key": "weight",
+    "variant_value": "500",
+    "display_value": "500gr"
+  },
+  {
+    "variant_key": "weight",
+    "variant_value": "700",
+    "display_value": "700gr"
+  },
+  {
+    "variant_key": "weight",
+    "variant_value": "900",
+    "display_value": "900gr"
+  }
+];
+
+const flavours = [
+  {
+    "variant_key": "flavour",
+    "variant_value": "vani",
+    "display_value": "Vani"
+  },
+  {
+    "variant_key": "flavour",
+    "variant_value": "chocolate",
+    "display_value": "Chocolate"
+  },
+  {
+    "variant_key": "flavour",
+    "variant_value": "strawberry",
+    "display_value": "Dâu"
+  },
+];
+
+const prices = ["150000", "200000", "300000", "100000", "500000"];
+var content = "";
+function createVariants () {
+  const baseQuery = "INSERT INTO `vp__product_variants` (`id`, `uuid`, `product_uuid`, `variant`, `price`, `image`, `active_status`) VALUES"
+  for (productKey in products) {
+    for (volumnKey in volumns) {
+      for (weightKey in weights) {
+        for (flavourKey in flavours) {
+          var uuid = generateUUID();
+          var variantArray = [];
+          variantArray.push(volumns[volumnKey]);
+          variantArray.push(weights[weightKey]);
+          variantArray.push(flavours[flavourKey]);
+          const price = prices[Math.floor(Math.random() * prices.length)];
+          var variantArrayJson = JSON.stringify(variantArray);
+          const valueQuery = `(NULL, '${uuid}', '${products[productKey]}', '${variantArrayJson}', '${price}', '1568387304_5d7bb0e84a55d', 'on');`;
+          query = baseQuery + valueQuery;
+          content = content + query + "\n";
+        }
+      }
+    }
+  }
+}
+function writeToOutput(fileOutput, content) {
+  var outputWriteStream = fs.createWriteStream(fileOutput);
+  outputWriteStream.write(content);
+}
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+console.log(generateUUID());
+createVariants();
+writeToOutput("query.txt", content);
